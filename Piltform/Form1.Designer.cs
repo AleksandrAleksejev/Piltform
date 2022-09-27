ed78d7c0afd194b8f1f8ae94ededa318338f9a87
opilane <opilane@HP-CZC6297B9B>
2022-09-27 15:04:30 +0300

    Add project files.

---
+++ Piltform/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Piltform
+{
+    partial class Pildid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel3.Controls.Add(this.pictureBox2, 0, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(568, 483);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tableLayoutPanel3.SetColumnSpan(this.pictureBox2, 2);
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(562, 428);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Pildid
+            // 
+            this.ClientSize = new System.Drawing.Size(567, 480);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Name = "Pildid";
+            this.Load += new System.EventHandler(this.Pildid_Load);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Piltform/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Piltform
+{
+    public partial class Pildid : Form
+    {
+        TableLayoutPanel tableLayoutPanel;
+        PictureBox pictureBox;
+        Button close_btn;
+        Button clear_btn;
+        Button show_btn;
+        FlowLayoutPanel flowLayoutPanel;
+
+
+
+        public Pildid()
+        {
+            this.Size = new System.Drawing.Size(900, 500);
+            this.Text = "Pildid";
+            tableLayoutPanel = new TableLayoutPanel
+            {
+                AutoSize = true,
+                ColumnCount = 2,
+                RowCount = 2,
+                Location = new System.Drawing.Point(0, 0),
+                Size = new System.Drawing.Size(900, 500),
+                TabIndex = 0,
+                BackColor = System.Drawing.Color.White,
+            };
+            tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle
+                (System.Windows.Forms.SizeType.Percent, 15F));
+            tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle
+               (System.Windows.Forms.SizeType.Percent, 85F));
+            tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.RowStyle
+               (System.Windows.Forms.SizeType.Percent, 90F));
+            tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.RowStyle
+               (System.Windows.Forms.SizeType.Percent, 5F));
+            tableLayoutPanel.ResumeLayout(false);
+
+            this.Controls.Add(tableLayoutPanel);
+
+            pictureBox = new System.Windows.Forms.PictureBox
+            {
+                BorderStyle = BorderStyle.Fixed3D,
+                Dock = DockStyle.Fill,
+                Location = new Point(3, 3),
+                Name = "PictureBox",
+                Size = new Size(562, 428),
+                TabIndex = 0,
+                TabStop = false,
+            };
+            tableLayoutPanel.Controls.Add(pictureBox,0,0);
+            tableLayoutPanel.SetCellPosition(pictureBox,new TableLayoutPanelCellPosition(0,0));
+            tableLayoutPanel.SetColumnSpan(pictureBox,2);
+
+            checkBox1 = new CheckBox {
+            Text = "Venita",
+            Dock = System.Windows.Forms.DockStyle.Fill,
+            
+            
+            
+            
+            };
+            tableLayoutPanel.Controls.Add(checkBox1,1,0);
+
+            this.Controls.Add(tableLayoutPanel);
+
+            close_btn = new Button
+            {
+                Text = "",
+                Dock = System.Windows.Forms.DockStyle.Fill,
+            };
+            clear_btn = new Button
+            {
+                Text = "Kustuta",
+                Dock = System.Windows.Forms.DockStyle.Fill,
+            };
+            show_btn = new Button
+            {
+                Text = "Naita",
+                Dock = System.Windows.Forms.DockStyle.Fill,
+            };
+            Button[] buttons = { clear_btn, show_btn, close_btn };
+            flowLayoutPanel = new FlowLayoutPanel
+            {
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                FlowDirection = FlowDirection.RightToLeft,
+                AutoSize= true,
+                WrapContents = false,
+                AutoScroll= true,
+            };
+            
+            foreach (Button button in buttons)
+            {
+                flowLayoutPanel.Controls.Add(button);
+            }
+            tableLayoutPanel.Controls.Add(flowLayoutPanel, 1, 1);
+            this.Controls.Add(tableLayoutPanel);
+
+
+        }
+       
+
+        private void Pildid_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
