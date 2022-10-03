87b4e40a6508247397c8e79346399c9d01439bb7
opilane <opilane@HP-CZC6297B82>
2022-10-03 13:15:52 +0300

    complete

--- Piltform/Form1.cs
+++ Piltform/Form1.cs
@@ -14,7 +14,10 @@ namespace Piltform
     {
         TableLayoutPanel tableLayoutPanel;
         PictureBox pictureBox;
-        Button close_btn;
+        CheckBox checkBox;
+        Button close_btn, bgColor, clear, showPicture;
+        ColorDialog colordialog;
+        OpenFileDialog openfiledialog;
         Button clear_btn;
         Button show_btn;
         FlowLayoutPanel flowLayoutPanel;
@@ -61,49 +64,74 @@ namespace Piltform
             tableLayoutPanel.SetCellPosition(pictureBox,new TableLayoutPanelCellPosition(0,0));
             tableLayoutPanel.SetColumnSpan(pictureBox,2);
 
-            checkBox1 = new CheckBox {
-            Text = "Venita",
-            Dock = System.Windows.Forms.DockStyle.Fill,
-            
-            
-            
-            
-            };
-            tableLayoutPanel.Controls.Add(checkBox1,1,0);
+            checkBox = new CheckBox
+            {
+                AutoSize = true,
+                Location = new System.Drawing.Point(150, 278),
+                TabIndex = 1,
+                UseVisualStyleBackColor = true,
+                Text = "Venita",
+                Dock = System.Windows.Forms.DockStyle.Fill,
 
-            this.Controls.Add(tableLayoutPanel);
+            };
+            checkBox.CheckedChanged += new System.EventHandler(CheckBox_CheckedChanged);
+            tableLayoutPanel.Controls.Add(checkBox);
 
             close_btn = new Button
             {
-                Text = "",
+                Text = "Suleda",
+                TabIndex = 1,
                 Dock = System.Windows.Forms.DockStyle.Fill,
             };
-            clear_btn = new Button
+            colordialog = new ColorDialog
             {
+                AllowFullOpen = true,
+                AnyColor = true,
+                SolidColorOnly = false,
+                Color = Color.Red,
+            };
+
+            bgColor = new Button
+            {
+                AutoSize = true,
+                TabIndex = 1,
+                Text = "Valida tausta värvi",
+                UseVisualStyleBackColor = true,
+
+            };
+            tableLayoutPanel.Controls.Add(bgColor);
+            this.bgColor.Click += new System.EventHandler(this.bgColor_Click);
+
+            clear = new Button
+            {
+                AutoSize = true,
+                TabIndex = 2,
                 Text = "Kustuta",
-                Dock = System.Windows.Forms.DockStyle.Fill,
+                UseVisualStyleBackColor = true,
             };
-            show_btn = new Button
+            tableLayoutPanel.Controls.Add(clear);
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+
+            showPicture = new Button
             {
-                Text = "Naita",
-                Dock = System.Windows.Forms.DockStyle.Fill,
+                AutoSize = true,
+                TabIndex = 3,
+                Text = "Näita pilti",
+                UseVisualStyleBackColor = true,
+
             };
-            Button[] buttons = { clear_btn, show_btn, close_btn };
-            flowLayoutPanel = new FlowLayoutPanel
+            tableLayoutPanel.Controls.Add(showPicture);
+            this.showPicture.Click += new System.EventHandler(this.showPicture_Click);
+
+            openfiledialog = new OpenFileDialog
             {
-                Dock = System.Windows.Forms.DockStyle.Fill,
-                FlowDirection = FlowDirection.RightToLeft,
-                AutoSize= true,
-                WrapContents = false,
-                AutoScroll= true,
+                RestoreDirectory = true,
+                Title = "Browse Text Files",
+                Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" + "s (*.*)|*.*",
+
             };
-            
-            foreach (Button button in buttons)
-            {
-                flowLayoutPanel.Controls.Add(button);
-            }
-            tableLayoutPanel.Controls.Add(flowLayoutPanel, 1, 1);
-            this.Controls.Add(tableLayoutPanel);
+
+
 
 
         }
@@ -112,6 +140,37 @@ namespace Piltform
         private void Pildid_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void bgColor_Click(object sender, EventArgs e)
+        {
+            if (colordialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.BackColor = colordialog.Color;
+            }
+        }
+        private void clear_Click(object sender, EventArgs e)
+        {
+            pictureBox.Image = null;
+        }
+        private void showPicture_Click(object sender, EventArgs e)
+        {
+            if (openfiledialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.Load(openfiledialog.FileName);
+            }
+        }
+        private void CheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox.Checked)
+            {
+                pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else { pictureBox.SizeMode = PictureBoxSizeMode.Normal; }
         }
     }
 }
